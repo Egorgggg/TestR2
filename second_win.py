a49d0c917943fd0c615a02ed0c9c86c7776241ef
Egorgggg <49187199+Egorgggg@users.noreply.github.com>
2024-04-18 16:44:55 +0300

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,8 +1,8 @@
 from PyQt5.QtCore import Qt,QTimer,QTime
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
 from instr import *
 from final_win import FinalWin
-
 class SecondWin(QWidget):
     def __init__(self):
         super().__init__()
@@ -20,13 +20,13 @@ class SecondWin(QWidget):
         self.fio = QLabel(txt_hintname)
         self.age = QLabel(txt_age)
         self.instruction1, self.instruction2, self.instruction3 = QLabel(txt_test1), QLabel(txt_test2), QLabel(txt_test3)
-        self.time = QLabel(txt_timer)
+        self.text_timer = QLabel(txt_timer)
         self.button1, self.button2, self.button3, self.nextbutton = QPushButton(txt_starttest1), QPushButton(txt_starttest2), QPushButton(txt_starttest3), QPushButton(txt_sendresults)
         self.fioedit, self.ageedit, self.check1, self.check2, self.check3 = QLineEdit(), QLineEdit(txt_hintage), QLineEdit(), QLineEdit(), QLineEdit()
         self.h_line = QHBoxLayout()
         self.r_line = QVBoxLayout()
         self.l_line = QVBoxLayout()
-        self.r_line.addWidget(self.time, alignment = Qt.AlignRight)        
+        self.r_line.addWidget(self.text_timer, alignment = Qt.AlignRight)        
         self.l_line.addWidget(self.fio, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.fioedit, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.age, alignment = Qt.AlignLeft)
@@ -37,6 +37,7 @@ class SecondWin(QWidget):
         self.l_line.addWidget(self.instruction2, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.button2, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.instruction3, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.button3, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.check2, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.check3, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.nextbutton, alignment = Qt.AlignCenter)
@@ -52,11 +53,11 @@ class SecondWin(QWidget):
     
     def next_click(self):    
         self.hide()
-        self.tw = FinalWin()
+        self.tw = FinalWin(self.ageedit.text(),self.check1.text(),self.check2.text(),self.check3.text())
     
     def timer_test(self):
         global time
-        time = QTime(0,1,0)
+        time = QTime(0,0,15)
         self.timer = QTimer()
         self.timer.timeout.connect(self.timer1Event)
         self.timer.start(1000)
@@ -72,7 +73,7 @@ class SecondWin(QWidget):
 
     def timer_sits(self):
         global time
-        time = QTime(0)
+        time = QTime(0,0,0)
         self.timer = QTimer()
         self.timer.timeout.connect(self.timer2Event)
         self.timer.start(1500)
@@ -98,9 +99,13 @@ class SecondWin(QWidget):
         global time
         time = time.addSecs(-1)
         self.text_timer.setText(time.toString('hh:mm:ss'))        
-        if int(time.toString('hh:mm:ss'[6:8]))>=45 or int(time.toString('hh:mm:ss'[6:8]))<=15:
-            self.text_timer.setStyleSheet('color:rgb(0,255,0)')
+        if int(time.toString('ss'))>=45 or int(time.toString('ss'))<=15:
+            self.text_timer.setStyleSheet('color:rgb(255,0,0)')
         else:
             self.text_timer.setStyleSheet('color:rgb(0,0,0)')
         if time.toString('hh:mm:ss') == '00:00:00':
             self.timer.stop()
+
+app = QApplication([])
+main_win = SecondWin()
+app.exec_()
